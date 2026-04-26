fe0cfb4e282a8d3197794a79b05aa4294ca2ca3e
mpragmas <mpragmas@gmail.com>
2026-04-26 23:57:22 +0200

    Fix: enabling cors for frontend

--- Program.cs
+++ Program.cs
@@ -42,7 +42,7 @@ builder.Services.AddAuthorization();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
-        policy.WithOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://legal-case-system-rda.vercel.app")
               .AllowAnyMethod()
               .AllowAnyHeader());
 });
